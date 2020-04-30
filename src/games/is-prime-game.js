bbdb89451a750ff110945fc76d4c686259faf9a6
Artem Kalachyan <ua.bringoff@gmail.com>
2020-04-30 18:36:55 +0300

    Guard if number is prime check

--- src/games/is-prime-game.js
+++ src/games/is-prime-game.js
@@ -5,11 +5,13 @@ import launchGame from '../game-engine.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  const firstPrime = 2;
+  if (num < firstPrime) return false;
   const root = Math.sqrt(num);
-  for (let i = 2; i <= root; i += 1) {
+  for (let i = firstPrime; i <= root; i += 1) {
     if (num % i === 0) return false;
   }
-  return num > 1;
+  return false;
 };
 
 const generateQuestionItem = () => {
